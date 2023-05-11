256eafe2570e47c1a783eb2df66ecd0ca9e1aa61
OlegforeverUkr <130793460+OlegforeverUkr@users.noreply.github.com>
2023-05-11 21:18:51 +0300

    Update Group_project.py
    Добавил возможность играть с компом

--- Group_project.py
+++ Group_project.py
@@ -1,23 +1,33 @@
-koloda = [6,7,8,9,10,2,3,4,11] *4
 import random
+
+koloda = [6, 7, 8, 9, 10, 2, 3, 4, 11] * 4
 random.shuffle(koloda)
 print('Lets play Black Jack!')
 count = 0
+comp = koloda.pop() * 2
 
 while True:
-    choice = input('Will you take a card? y/n\n')
+    choice = input('Will you take a card? y/n - ')
+    print(f'Casino have - {comp}')
+    if comp > 21:
+        print("You win!!!")
     if choice == 'y':
         current = koloda.pop()
-        print('You got a card of value %d' %current)
+        print('You got a card of value %d' % current)
         count += current
         if count > 21:
-            print('Sorry! You lose')
+            print(f'Sorry! You lose. You have {count}')
             break
         elif count == 21:
             print('Сongrat! You have 21')
             break
         else:
-            print('You have %d point' %count)
+            print('You have %d point' % count)
     elif choice == 'n':
-        print('You have %d points and you finished game' %count)
+        while comp < 16 and comp < count:
+            comp += koloda.pop()
+        if comp > count and comp <= 21:
+            print(f"You lose. Casino have {comp}")
+        elif count > comp:
+            print("You win!!")
         break
